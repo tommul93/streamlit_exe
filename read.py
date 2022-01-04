df1fd2d07a0517d7bde90326b1e97ed3ccb42fd3
Tom Mul <t.mul@brightcape.nl>
2022-01-04 10:14:09 +0100

    eng

--- read.py
+++ read.py
@@ -4,6 +4,8 @@ import streamlit as st
 
 #poetry export -f requirements.txt --output requirements.txt --without-hashes
 
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 st.title("Hello!")
 st.text("This is an example application that test the ability to create an .exe from an web-application")
 
@@ -13,7 +15,9 @@ image = st.file_uploader("upload file", type=["png","jpg"])
 if image:
     img = Image.open(image)
     st.image(img, width=350)
-    st.info("extracted text")
+    st.info("extracted text : ")
+    text = pytesseract.image_to_string(img, lang="eng")
 
-    text = pytesseract.image_to_string(img, lang="en")
+    # pytesseract.image_to_data(img, output_type=pytesseract.Output.DATAFRAME)
     st.write("{}".format(text))
+
